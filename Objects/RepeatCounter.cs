e74d14767eb5595774893e8676e15617a4d3caca
Anthony Bottemiller <anthonybottemiller@outlook.com>
2016-12-02 10:42:14 -0800

    add argument to split method on line 8 better explicit than implicit

--- Objects/RepeatCounter.cs
+++ Objects/RepeatCounter.cs
@@ -5,7 +5,7 @@ namespace RepeatCounter
     private int _wordCount = 0;
     public int Count(string inputString, string comparisonString)
     {
-      string[] words = inputString.Split();
+      string[] words = inputString.Split(' ');
       foreach (string word in words)
       {
         if (word == comparisonString)
