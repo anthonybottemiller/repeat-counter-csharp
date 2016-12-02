858cf82960cfa7c47b813159acd463ce74d323cd
Anthony Bottemiller <anthonybottemiller@outlook.com>
2016-12-02 10:04:43 -0800

    change name of first test to increase clarity

--- Tests/RepeatCounterTest.cs
+++ Tests/RepeatCounterTest.cs
@@ -4,7 +4,7 @@ namespace RepeatCounter
   public class RepeatCounterTest
   {
     [Fact]
-    public void RepeatCounter_ForStringsThatDontMatch_0()
+    public void RepeatCounter_ForTwoWordsThatDontMatch_0()
     {
       RepeatCounter testRepeatCounter = new RepeatCounter();
       Assert.Equal(0, testRepeatCounter.Count("c#","java"));
