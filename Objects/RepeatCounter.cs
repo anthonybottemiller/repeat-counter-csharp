e4f63b59234b8042557910f5614e55167d4f90d6
Anthony Bottemiller <anthonybottemiller@outlook.com>
2016-12-02 10:01:31 -0800

    add model for RepeatCounter it passes first spec

---
+++ Objects/RepeatCounter.cs
@@ -0,0 +1,11 @@
+namespace RepeatCounter
+{
+  public class RepeatCounter
+  {
+    private int _wordCount = 0;
+    public int Count(string inputString, string comparisonString)
+    {
+      return _wordCount;
+    }
+  }
+}
