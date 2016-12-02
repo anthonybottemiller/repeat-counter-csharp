a2ca80fe381e4184f56bce26574d74830e47d342
Anthony Bottemiller <anthonybottemiller@outlook.com>
2016-12-02 12:13:44 -0800

    update model with two properties containing user input to be initialized upon construction

--- Objects/RepeatCounter.cs
+++ Objects/RepeatCounter.cs
@@ -2,13 +2,20 @@ namespace RepeatCounter
 {
   public class RepeatCounter
   {
+    private string _inputString;
+    private string _comparisonString;
     private int _wordCount = 0;
-    public int Count(string inputString, string comparisonString)
+    public RepeatCounter(string inputString, string comparisonString)
     {
-      string[] words = inputString.Split(' ');
+      _inputString = inputString;
+      _comparisonString = comparisonString;
+    }
+    public int Count()
+    {
+      string[] words = _inputString.Split(' ');
       foreach (string word in words)
       {
-        if (word == comparisonString)
+        if (word == _comparisonString)
         {
           _wordCount++;
         }
